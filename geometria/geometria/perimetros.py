f50920494227f763a9e52aa8d7dae6df206d33f5
JuanJG01 <158237017+JuanJG01@users.noreply.github.com>
2024-03-11 19:32:31 -0700

    practicas y tareas de la materia de python

---
+++ geometria/geometria/perimetros.py
@@ -0,0 +1,8 @@
+def perimetro_cuadrado(lado):
+    return 4 * lado
+
+def perimetro_triangulo(lado1, lado2, lado3):
+    return lado1 + lado2 + lado3
+
+def perimetro_circulo(radio):
+    return 2 * 3.14159 * radio
---
+++ importacion/raiz.py
@@ -0,0 +1,6 @@
+from math import sqrt
+
+numero = 16
+raiz_cuadrada = sqrt(numero)
+
+print("La raíz cuadrada de", numero, "es:", raiz_cuadrada)
