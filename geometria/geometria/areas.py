094d46debe729a1b9b0e7470be05e4d83d5a499e
JuanJG01 <158237017+JuanJG01@users.noreply.github.com>
2024-03-11 19:33:42 -0700

    Delete geometria directory

--- geometria/geometria/areas.py
+++
@@ -1,8 +0,0 @@
-def area_cuadrado(lado):
-    return lado ** 2
-
-def area_triangulo(base, altura):
-    return (base * altura) / 2
-
-def area_circulo(radio):
-    return 3.14159 * radio ** 2
--- geometria/geometria/perimetros.py
+++
@@ -1,8 +0,0 @@
-def perimetro_cuadrado(lado):
-    return 4 * lado
-
-def perimetro_triangulo(lado1, lado2, lado3):
-    return lado1 + lado2 + lado3
-
-def perimetro_circulo(radio):
-    return 2 * 3.14159 * radio
